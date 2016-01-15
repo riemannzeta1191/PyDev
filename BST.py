361d6c932b7d19ca3c25e4f950e1b714c10cab2b
Sayan Bhattacharya <sayanbhattacharya11@gmail.com>
2016-01-15 14:06:15 +0530

    Initial commit

---
+++ BST.py
@@ -0,0 +1,14 @@
+class BST(object):
+    def __init__(self,root,size=None):
+        self._root = root
+        self._size = size
+
+    class Node(object):
+        def __init__(self,val):
+
+            self._val  = val
+
+    def _insertNode_(self,root,size,Node):
+        if self.root is None:
+            newNode = Node(Node.val)
+        elif self.Node._val > self.Node.    
